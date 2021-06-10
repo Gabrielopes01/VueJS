d17dfbd49a039e642601bf8301afe0ae39cad760
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-06-10 11:28:02 -0300

    Componentes e CLI

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Teste Data'
+    }
+  }
+}
+</script>
+
+<style>
+  #app{
+    background-color: aqua;
+  }
+</style>
+
