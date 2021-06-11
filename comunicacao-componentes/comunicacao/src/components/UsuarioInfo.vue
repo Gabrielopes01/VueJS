fa55a8340d5ae366ede6a0e7ba6755553a075d07
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-06-11 11:10:56 -0300

    Componentes

---
+++ comunicacao-componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="adicionarSimbolo">Adicionar !</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            // required: true
+            default: 'Anônimo'
+        },
+        adicionarSimbolo: Function,
+        idade: Number
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            const antigo = this.nome
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', {
+                novo: this.nome,
+                antigo
+            })
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
