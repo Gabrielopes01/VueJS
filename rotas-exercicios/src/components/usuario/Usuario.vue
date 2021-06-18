b323f673b0bde358a210282a47f7445a8aefaab3
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-06-18 14:27:36 -0300

    Vue Router

---
+++ rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="usuario">
+      <h2>Usuarios</h2>
+      <hr>
+        <router-view></router-view>
+      <button sucesso @click="inParaInicio">Voltar</button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        inParaInicio() {
+            // this.$router.push('/')
+            //this.$router.push({ path: '/' })
+            this.$router.push({ name: 'inicio' })
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="usuario-detalhe">
+      <h3>Usuário Detalhe</h3>
+      <p><strong>Código: </strong>{{ id }}</p>
+      <!-- :to="`/usuario/${id}/editar`" -->
+      <router-link tag="button" primario
+        :to="{name: 'editarUsuario', params: { id: $route.params.id },
+            query: { completo: true, lingua: 'pt' },
+            hash: '#rodape' }">
+          Editar
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    // data() {
+    //     return {
+    //         id: this.$route.params.id   //Parametro da rota
+    //     }
+    // },
+    // watch: {
+    //     $route(to, from) {
+    //         this.id = to.params.id
+    //     }
+    // },
+}
+</script>
+
+<style>
+
+</style>
