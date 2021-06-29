8654af28aaf49b6534b34f21298d60ec140f9544
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-06-29 08:36:50 -0300

    Projeto Calculadora

---
+++ calculator/src/components/Display.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="display">
+      {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['value']
+}
+</script>
+
+<style>
+.display {
+    grid-column: span 4;
+    background-color: #0004;
+
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: flex-end;
+    padding: 10px;
+    font-size: 25px;
+}
+</style>
