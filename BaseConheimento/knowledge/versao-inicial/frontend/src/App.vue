61cf44e9c298413092d0dad914b1e30c8d06f03d
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-07-01 14:44:49 -0300

    Inicio do FronteEnd

--- BaseConheimento/knowledge/versao-inicial/frontend/src/App.vue
+++ BaseConheimento/knowledge/versao-inicial/frontend/src/App.vue
@@ -5,6 +5,11 @@
 </template>
 
 <script>
+import Header from './components/template/Header'
+import Menu from './components/template/Menu'
+import Content from './components/template/Content'
+import Footer from './components/template/Footer'
+
 export default {
 	name: "App",
 }
